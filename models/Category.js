c924b5f239574f01f86d617f5989db716f02c585
imran-rassi <imran.rassi.2020@gmail.com>
2022-11-26 23:19:16 -0500

    added the columns in Category and Product.js

--- models/Category.js
+++ models/Category.js
@@ -5,9 +5,23 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
+
   {
-    // define columns
+    // define the columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   },
+
   {
     sequelize,
     timestamps: false,
@@ -15,6 +29,7 @@ Category.init(
     underscored: true,
     modelName: 'category',
   }
+  
 );
 
 module.exports = Category;
--- models/Product.js
+++ models/Product.js
@@ -8,9 +8,52 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+
   {
-    // define columns
+    // define the columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: "category",
+        key: "id"
+      }
+
+    }
+
   },
+
   {
     sequelize,
     timestamps: false,
@@ -18,6 +61,7 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
+  
 );
 
 module.exports = Product;
